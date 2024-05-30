15156825c5591672e8e67c9dba64aad2c9e17030
Yen-Ting Li <ytli.tw@gmail.com>
2024-05-30 17:24:26 +0800

    fix: starting script

--- essay_judge/main.py
+++ essay_judge/main.py
@@ -1,18 +1,20 @@
 from argparse import ArgumentParser 
-from llm import chain
-from mail import Mail, send_mail
-from reader import read_file
+from .llm import chain
+from .mail import Mail, send_mail
+from .reader import read_file
 
-parser = ArgumentParser()
-parser.add_argument("essay_path", nargs='?',  help="The essay document", default="")
-args = parser.parse_args()
 
-essay_content = read_file(args.essay_path)
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("essay_path", nargs='?',  help="The essay document", default="")
+    args = parser.parse_args()
 
-answer = chain.invoke({"input": essay_content})
-print(answer)
+    essay_content = read_file(args.essay_path)
 
-mail = Mail(to="r12227113@ntu.edu.tw", subject="評語", content="評語：" + answer.content)
-res = send_mail(mail)
-print(res)
+    answer = chain.invoke({"input": essay_content})
+    print(answer)
+
+    mail = Mail(to="r12227113@ntu.edu.tw", subject="評語", content="評語：" + answer.content)
+    res = send_mail(mail)
+    print(res)
 
